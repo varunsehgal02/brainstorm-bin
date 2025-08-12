d0040854e068233a51d51ecf3caa8beba9101269
varunsehgal02 <varun.sehgal02@gmail.com>
2025-08-12 21:37:41 +0530

    create a object

---
+++ Js Object/index.js
@@ -0,0 +1,9 @@
+const JsUser = {
+    name:"varun",
+    age: 20,
+    location: "Guna",
+    email : "varun.sehgal02@gmail.com",
+    isloggedIn : "fales",
+    lastLoginDays: ["Monday", "Saturday"]
+};
+
