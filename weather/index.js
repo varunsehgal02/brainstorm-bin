309713473839a9cb5e11bda7d35b47b04fadcd7b
varunsehgal02 <varun.sehgal02@gmail.com>
2025-08-12 21:25:11 +0530

    added more data in js  file

--- weather/index.js
+++ weather/index.js
@@ -1,6 +1,10 @@
 let p =  fetch("https://goweather.herokuapp.com/weather/Ny");
 p.then((response) => {
+    console.log(response.status);
+    console.log(response.ok);
+    console.log(response.headers);
     return response.json();
+
 }).then((response) => {
     console.log(response);
 });
