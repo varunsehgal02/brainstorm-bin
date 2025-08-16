1065534f9584bc46f0b808d8b99426c5a0ff2342
varunsehgal02 <varun.sehgal02@gmail.com>
2025-08-16 14:30:11 +0530

    added a Symbol

--- Js Object/index.js
+++ Js Object/index.js
@@ -1,14 +1,18 @@
+const mySym = Symbol("key1");
 const JsUser = {
     name:"varun",
     "Full Name" : "Varun Sehgal",
     age: 20,
     location: "Dabra",
+    [mySym] : "key1" ,
     email : "varunsehgal2005@gmail.com",
     isloggedIn : "fales",
     lastLoginDays: ["Monday", "Saturday"]
 };
 
 console.log(JsUser.email);
+// console.log(JsUser.mySym);
+console.log(JsUser[mySym]);
 console.log(JsUser["email"]);
 console.log(JsUser["Full Name"]);
 console.log("Hello Varun")
